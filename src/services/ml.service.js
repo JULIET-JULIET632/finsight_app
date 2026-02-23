96b9930d5a12eeec7621a3e6e569a6852296da58
Mercedes💕 <mercedesmpieri@Mercedes-MacBook-Pro.local>
2026-02-23 14:13:19 +0100

    chore: removed duplicate root files to finalize monorepo structure

--- src/services/ml.service.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const PYTHON_API_URL = process.env.PYTHON_API_URL || "http://127.0.0.1:8000";
-
-export const runPrediction = async (businessData) => {
-  const response = await axios.post(`${PYTHON_API_URL}/predict, businessData`);
-  return response.data;
-};
